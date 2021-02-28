252818418f2bded3090d2a9c851ecbb913130419
fadlyhdiwjya <wijayafadlyhadi28@gmail.com>
2021-02-28 12:05:25 +0700

    add controller post data

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,9 +14,27 @@ exports.getStudentsById = (req, res) => {
     let id = req.params.id;
 
     let query = `SELECT * FROM tbl_mahasiswa WHERE id = ${id}`;
-    db.query(query, (err, rows, fields) => {
+    db.query(query, (err, result, fields) => {
         if(err) throw err;
-         res.json({error: false, result: rows});
+         res.json({error: false, result: result});
     })
+}
+
+exports.addStudents = ( req, res, next ) => {
+
+    let form = {
+        nama: req.body.nama,
+        nim : req.body.nim,
+        jurusan: req.body.jurusan
+    }
+
+    let sql = `INSERT INTO tbl_mahasiswa ( id, nama, nim, jurusan) VALUES (null, ?,?,?)`;
+    db.query(sql, [form.nama, form.nim, form.jurusan], (err, result) => {
+        if(err) {
+            res.json({error: true, result: err});
+        } else {
+            res.json({error: false, result: result});
+        }
+    });
 }
 
